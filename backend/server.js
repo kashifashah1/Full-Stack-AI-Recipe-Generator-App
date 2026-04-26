ba15b2892df29c6b4fc10a0e32e6c5639c88189d
kashifashah1 <kashifa.shah24@gmail.com>
2026-04-26 22:15:28 +0530

    Project Added with api for user and its preferences

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import  dotenv from 'dotenv'
+
+dotenv.config();
+
+import express from 'express'
+import cors from 'cors';
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+
+const app = express(); 
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Test route
+app.get('/',(req,res)=>{
+    res.json({message:'AI Recipe Generator API'})
+})
+
+//API Routes
+app.use('/api/auth',authRoutes);
+app.use('/api/users',userRoutes);
+
+const PORT=process.env.PORT || 8000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+    console.log(`Environments: ${process.env.NODE_ENV || 'development'}`);
+      
+})
